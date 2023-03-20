fce25d7d5ad49253ffa7e77a9863e663a7c3327b
lucas <lucasgabrielaraujo13.08@gmail.com>
2023-03-20 20:22:26 -0300

    get de app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,7 @@
-console.log("Programa de ejemplo");
+const express = require('express');                                     //trae la funcion desde la libreria
+const app = express();                                                  //ejecuta la funcion
+const PORT = 3001;                                                      //GET, POST, DELETE, PUT, PATCH - Verbos http
+app.get('/api', (req,res)=>{                                            //
+    res.json({mensaje:'hola mundo'}).status(200)
+});
+app.listen(PORT,()=>{console.log('estoy escuchando en el puerto: '+PORT)});
